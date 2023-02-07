276ecb4084f4969c418e0c333c47c5ec33635058
Gcolin1 <86110731+Gcolin1@users.noreply.github.com>
2023-02-07 14:51:08 -0300

    Add files via upload

--- querys/lista1.sql
+++ querys/lista1.sql
@@ -31,3 +31,16 @@ INSERT INTO cursos (nome, mensalidade) VALUES
 ('medicina', 1500);
 
 SELECT * FROM cursos
+
+INSERT INTO alunos(nome, cpf, email, data_nascimento, endereco, complemento) VALUES
+('guilherme', 'guilherme@email.com', '03-03-2002', '', 'casa'),
+('fernando', 'fernando@email.com', '02-05-2004', 'rua nordestina', 'apartamento'),
+('agatha', 'agatha@email.com', '01-04-2000', 'rua 1', ''),
+('nicolly', 'nicolly@email.com', '02-02-2005', 'rua 22', 'apartamento'),
+('matheus', 'matheus@email.com', '01-05-2005', 'rua 21', 'casa'),
+('vitor', 'vitor@email.com', '03-01-1990', 'rua 90', 'apartamento'),
+('gustavo', 'gustavo@email.com', '31-12-1999', 'rua 34', 'casa'),
+('geovana', 'geovana@email.com', '29-02-2003', 'rua 112', 'casa'),
+('julia', 'julia@email.com', '22-02-2002', 'rua 205', 'casa'),
+('giovani', 'giovani@email.com', '26-012-1980', 'rua 12', 'casa'),
+('felipe', 'felipe@email.com', '10-12-1998', 'rua 4', 'apartamento');
